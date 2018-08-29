baf3dce9113996635981ec13769ddf5a9a48b642
Bas Paap <baspaap@live.nl>
2018-08-30 00:31:58 +0200

    Added Character tests

--- Bas.RedYarn.Tests/CharacterTest.cs
+++ Bas.RedYarn.Tests/CharacterTest.cs
@@ -28,5 +28,73 @@ namespace Bas.RedYarn
         {
             ToStringHelper.ToString_NameIsEmpty_ReturnsClassName(this.character);
         }
+                
+        [TestMethod]
+        public void StorylinesAdd_NewStoryline_StorylineContainsCharacter()
+        {
+            // Arrange
+            var storyline = new Storyline() { Name = "Storyline" };
+
+            // Act
+            this.character.Storylines.Add(storyline);
+            
+            // Assert          
+            Assert.AreSame(this.character, storyline.Characters[0]);            
+        }
+
+        [TestMethod]
+        public void StorylinesInsert_NewStoryline_StorylineContainsCharacter()
+        {
+            // Arrange
+            var storyline = new Storyline() { Name = "Storyline" };
+
+            // Act
+            this.character.Storylines.Insert(0, storyline);
+
+            // Assert          
+            Assert.AreSame(this.character, storyline.Characters[0]);
+        }
+
+        [TestMethod]
+        public void StorylinesClear_StorylineContainsCharacter_CharacterIsRemovedFromStoryline()
+        {
+            // Arrange
+            var storyline = new Storyline() { Name = "Storyline" };
+            this.character.Storylines.Add(storyline);
+
+            // Act
+            this.character.Storylines.Clear();
+
+            // Assert          
+            Assert.AreEqual(0, storyline.Characters.Count);
+        }
+
+        [TestMethod]
+        public void StorylinesRemove_StorylineContainsCharacter_CharacterIsRemovedFromStoryline()
+        {
+            // Arrange
+            var storyline = new Storyline() { Name = "Storyline" };
+            this.character.Storylines.Add(storyline);
+
+            // Act
+            this.character.Storylines.Remove(storyline);
+
+            // Assert          
+            Assert.AreEqual(0, storyline.Characters.Count);
+        }
+
+        [TestMethod]
+        public void StorylinesRemoveAt_StorylineContainsCharacter_CharacterIsRemovedFromStoryline()
+        {
+            // Arrange
+            var storyline = new Storyline() { Name = "Storyline" };
+            this.character.Storylines.Add(storyline);
+
+            // Act
+            this.character.Storylines.RemoveAt(0);
+
+            // Assert          
+            Assert.AreEqual(0, storyline.Characters.Count);
+        }
     }
 }
