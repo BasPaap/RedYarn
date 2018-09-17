5aa4bed11d63dfaa34b2045da34416eb04c1f9e3
Bas Paap <baspaap@live.nl>
2018-09-17 21:40:53 +0200

    Added XML documentation to StringExtensions.

--- Bas.RedYarn/Extensions/StringExtensions.cs
+++ Bas.RedYarn/Extensions/StringExtensions.cs
@@ -6,6 +6,11 @@ namespace Bas.RedYarn.Extensions
 {
     static class StringExtensions
     {
+        /// <summary>
+        /// Returns a trimmed version of originalString where various invalid characters are removed.
+        /// </summary>
+        /// <param name="originalString">The string value that possibly contains invalid characters and untrimmed whitespace.</param>
+        /// <returns>A trimmed version of originalString where various invalid characters are removed.</returns>
         public static string Sanitize(this string originalString)
         {
             string[] invalidCharacters = new [] { "\0", "\a", "\b", "\f", "\n", "\r", "\t" };
