44c39e003d8f5193466baa975af25746afc76d9f
Bas Paap <baspaap@live.nl>
2018-09-28 21:57:07 +0200

    Added Angular/Material

--- Bas.RedYarn.WebApp/ClientApp/src/app/app.module.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
