7226d7415128b7ddf95b401137b0e4b5881110cf
Bas Paap <baspaap@live.nl>
2018-09-30 20:50:31 +0200

    Added top toolbar

--- Bas.RedYarn.WebApp/ClientApp/src/app/app.component.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- Bas.RedYarn.WebApp/ClientApp/src/app/app.module.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatDialogModule } from '@angular/material';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginDialogComponent
+    LoginDialogComponent,
+    MainToolbarComponent
   ],
   imports: [
     BrowserModule,
