cfc03db7f8a31b13a2b3efdda39c9ac3396b32ed
Bas Paap <baspaap@live.nl>
2018-10-02 17:21:53 +0200

    Use custom theme for main-toolbar.

--- Bas.RedYarn.WebApp/ClientApp/src/app/main-toolbar/main-toolbar.component.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/main-toolbar/main-toolbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-toolbar',
   templateUrl: './main-toolbar.component.html',
-  styleUrls: ['./main-toolbar.component.css']
+  styleUrls: ['./main-toolbar.component.scss']
 })
 export class MainToolbarComponent implements OnInit {
 
