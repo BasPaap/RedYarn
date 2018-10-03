98d49089a720f0c22372cfca0aec156b2a7882e2
Bas Paap <baspaap@live.nl>
2018-10-03 09:42:33 +0200

    Add EssentialInformation

---
+++ Bas.RedYarn.Tests/EssentialInformationTest.cs
@@ -0,0 +1,34 @@
+﻿using Bas.RedYarn.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bas.RedYarn
+{
+    [TestClass]
+    public class EssentialInformationTest
+    {
+        private EssentialInformation essentialInformation;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.essentialInformation = new EssentialInformation();
+        }
+
+        #region ToString
+        [TestMethod]
+        public void ToString_NameIsNotEmpty_ReturnsName()
+        {
+            ToStringHelper.ToString_NameIsNotEmpty_ReturnsName(this.essentialInformation);
+        }
+
+        [TestMethod]
+        public void ToString_NameIsEmpty_ReturnsClassName()
+        {
+            ToStringHelper.ToString_NameIsEmpty_ReturnsClassName(this.essentialInformation);
+        }
+        #endregion
+    }
+}
---
+++ Bas.RedYarn/EssentialInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Bas.RedYarn
+{
+    /// <summary>
+    /// Represents information owned by characters or required for characters to 'complete' a storyline.
+    /// </summary>
+    public sealed class EssentialInformation : INameable
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get; set; }
+        public Collection<Storyline> Storylines { get; }
+        public Collection<Character> OwningCharacters { get; }
+        public Collection<Character> NeedingCharacters { get; }
+    }
+}
