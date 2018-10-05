24dc3760689993b041f56526380a6f3ea472ca98
Bas Paap <baspaap@live.nl>
2018-10-05 17:28:02 +0200

    Fix login dialog layout.

--- Bas.RedYarn.WebApp/ClientApp/src/app/app.module.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatDialogModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatCheckboxModule, MatInputModule, MatDialogModule, MatSidenavModule, MatListModule } from '@angular/material';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -24,6 +24,8 @@ import { StoryDiagramComponent } from './story-diagram/story-diagram.component';
     MatToolbarModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
     MatDialogModule,
     MatSidenavModule,
     MatListModule
