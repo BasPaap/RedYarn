bbb25499ec8a43ee4e46644316e4d7a03bfbea11
Bas Paap <baspaap@live.nl>
2018-10-10 11:44:06 +0200

    Fixed Sanitize() extension method for null strings.

--- Bas.RedYarn/Extensions/StringExtensions.cs
+++ Bas.RedYarn/Extensions/StringExtensions.cs
@@ -13,6 +13,11 @@ namespace Bas.RedYarn.Extensions
         /// <returns>A trimmed version of originalString where various invalid characters are removed.</returns>
         public static string Sanitize(this string originalString)
         {
+            if (originalString == null)
+            {
+                return null;
+            }
+
             string[] invalidCharacters = new [] { "\0", "\a", "\b", "\f", "\n", "\r", "\t" };
             var stringBuilder = new StringBuilder(originalString);
 
