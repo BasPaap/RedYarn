01afe2a4f8e368130f8d4d0192a6f59dd1f8ec39
Bas Paap <baspaap@live.nl>
2018-10-12 22:29:52 +0200

    Modify style -Show characters as circular images with default image -Disable strikethrough on plotElement arrows -Change character borders -Change Storyline appearance -Chage plotElement appearance -Refactor diagram-generator.service

--- Bas.RedYarn.WebApp/ClientApp/src/app/diagram-generator.service.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/diagram-generator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Diagram } from './diagram-types';
+import { Diagram, Character, Relationship, Storyline, StorylineCharacterConnection, StorylineEssentialPlotElementConnection, EssentialPlotElement } from './diagram-types';
 import { DataSet } from 'vis';
 
 @Injectable({
@@ -11,48 +11,99 @@ export class DiagramGeneratorService {
 
   generate(diagram: Diagram, nodes: DataSet<{}>, edges: DataSet<{}>) {
     for (let character of diagram.characters) {
-      nodes.add({ id: character.id, label: character.name, shape: 'circle' });
+      nodes.add(this.getCharacterNode(character));      
     }
 
     for (let relationship of diagram.relationships) {
-      edges.add({
-        arrows: relationship.isDirectional ? 'to' : undefined,
-        from: relationship.fromCharacterId,
-        to: relationship.toCharacterId,
-        label: relationship.name,
-        smooth: {
-          type: "continuous",
-          forceDirection: "none"
-        },
-        color: { color: 'rgba(255,0,0,1)', highlight: 'rgba(255,0,0,1)' },
-      });
+      edges.add(this.getRelationshipEdge(relationship));
     }
 
     for (let storyline of diagram.storylines) {
-      nodes.add({ id: storyline.id, label: storyline.name, shape: 'box' });
+      nodes.add(this.getStorylineNode(storyline));
     }
 
     for (let connection of diagram.storylineCharacterConnections) {
-      edges.add({
-        from: connection.connectionId,
-        to: connection.storylineId,
-        smooth: false,
-        color: { color: 'rgba(0,0,0,1)', highlight: 'rgba(0,0,0,1)' },
-      });
+      edges.add(this.getStorylineCharacterConnectionEdge(connection));
     }
 
     for (let essentialPlotElement of diagram.essentialPlotElements) {
-      nodes.add({ id: essentialPlotElement.id, label: essentialPlotElement.name, shape: 'box' });
+      nodes.add(this.getEssentialPlotElementNode(essentialPlotElement));
     }
 
     for (let connection of diagram.storylineEssentialPlotElementConnections) {
-      edges.add({
-        from: connection.connectionId,
-        to: connection.storylineId,
-        smooth: false,
-        arrows: 'to',
-        color: { color: 'rgba(0,0,0,0.5)', highlight: 'rgba(0,0,0,0.5)' },
-      });
-    }
+      edges.add(this.getStorylineEssentialPlotElementConnectionEdge(connection));
+    }    
+  }
+
+  private getEssentialPlotElementNode(essentialPlotElement: EssentialPlotElement) {
+    return {
+      id: essentialPlotElement.id,
+      label: essentialPlotElement.name,
+      shape: 'box',
+      color: {
+        border: 'rgba(50,50,50,0.8)',
+        background: 'rgba(200,200,200,1)',
+        highlight: { border: 'rgba(50,50,50,1)', background: 'rgba(200,200,200,1)' }
+      }
+    };
+  }
+
+  private getCharacterNode(character: Character) {
+    return {
+      id: character.id,
+      label: character.name,
+      shape: 'circularImage',
+      image: '404.jpg',
+      brokenImage: '../../../assets/default-character.png',
+      borderWidth: 2,
+      color: { border: 'rgba(0,0,0,0.8)', highlight: { border: 'rgba(0,0,0,1)' } }
+    };
+  }
+
+  private getRelationshipEdge(relationship: Relationship) {
+    return {
+      arrows: relationship.isDirectional ? 'to' : undefined,
+      from: relationship.fromCharacterId,
+      to: relationship.toCharacterId,
+      label: relationship.name,
+      smooth: {
+        type: "continuous",
+        forceDirection: "none"
+      },
+      color: { color: 'rgba(255,0,0,1)', highlight: 'rgba(255,0,0,1)' },
+    };
+  }
+
+  private getStorylineNode(storyline: Storyline) {
+    return {
+      id: storyline.id,
+      label: storyline.name,
+      shape: 'box',
+      color: {
+        border: 'rgba(0,0,0,0.8)',
+        background: 'rgba(255,255,255,1)',
+        highlight: { border: 'rgba(0,0,0,1)', background: 'rgba(255,255,255,1)' }
+      }
+    };
+  }
+
+  private getStorylineCharacterConnectionEdge(connection: StorylineCharacterConnection) {
+    return {
+      from: connection.connectionId,
+      to: connection.storylineId,
+      smooth: false,
+      color: { color: 'rgba(0,0,0,1)', highlight: 'rgba(0,0,0,1)' },
+    };
+  }
+
+  private getStorylineEssentialPlotElementConnectionEdge(connection: StorylineEssentialPlotElementConnection) {
+    return {
+      from: connection.connectionId,
+      to: connection.storylineId,
+      smooth: false,
+      arrows: 'to',
+      arrowStrikethrough: false,
+      color: { color: 'rgba(0,0,0,0.5)', highlight: 'rgba(0,0,0,0.5)' },
+    };
   }
 }
