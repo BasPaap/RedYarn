4ef65fb55b7330397263e4b994b754acfcbe4954
Bas Paap <baspaap@live.nl>
2018-10-15 17:11:38 +0200

    Add EF Core database project for WebApp.

---
+++ Bas.RedYarn.WebApp.Database/RedYarnDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bas.RedYarn.WebApp.Database
+{
+    public sealed class RedYarnDbContext : DbContext
+    {
+        public DbSet<Diagram> Diagrams { get; set; }
+        public DbSet<Node> Nodes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Diagram>().Property<Guid>(ShadowPropertyNames.Id)
+                                          .ValueGeneratedOnAdd()
+                                          .HasAnnotation("Key", 0);
+        }
+    }
+}
---
+++ Bas.RedYarn.WebApp.Database/StorylineNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bas.RedYarn.WebApp.Database
+{
+    public sealed class StorylineNode : INode
+    {
+        public Guid Id { get; set; }
+        public float XPosition { get; set; }
+        public float YPosition { get; set; }
+
+        public Storyline Storyline { get; set; }
+    }
+}
