52fb890540557e7a61f2601d7d58a6d72d629164
Bas Paap <baspaap@live.nl>
2018-10-17 22:04:46 +0200

    Implement generated update and delete controller action tests

--- Bas.RedYarn.WebApp.Tests/DiagramControllerTest.cs
+++ Bas.RedYarn.WebApp.Tests/DiagramControllerTest.cs
@@ -10,20 +10,6 @@ namespace Bas.RedYarn.WebApp.Tests
     [TestClass]
     public partial class DiagramControllerTest
     {
-        
-        // UpdateDiagram
-        // arg null -> 400 bad request
-        // id bestaat niet -> 404 not found
-        // vm is okee -> 204 no content (https://stackoverflow.com/questions/797834/should-a-restful-put-operation-return-something)
-
-        // DeleteDiagram
-        // id bestaat niet -> 404 not found
-        // id bestaat -> 204 no content
-
-        // GetDiagram
-        // id bestaat => 200
-        // id bestaat niet => 404 not found
-
         public void AssertCreatedDiagram(DiagramViewModel diagram)
         {           
             
