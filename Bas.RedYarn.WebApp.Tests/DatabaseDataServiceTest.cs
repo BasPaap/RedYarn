4de084a370636f5771fdc604c580cc039d0b189d
Bas Paap <baspaap@live.nl>
2018-10-24 19:16:30 +0200

    Reorganize class methods.

--- Bas.RedYarn.WebApp.Tests/DatabaseDataServiceTest.cs
+++ Bas.RedYarn.WebApp.Tests/DatabaseDataServiceTest.cs
@@ -10,12 +10,37 @@ namespace Bas.RedYarn.WebApp.Tests
     [TestClass]
     public class DatabaseDataServiceTest : DatabaseDataServiceTestBase
     {
+        protected override Author GetTestAuthor()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override AuthorViewModel GetTestAuthorViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
         protected override void AssertAuthor(Author model)
         {
             throw new NotImplementedException();
         }
 
         protected override void AssertAuthorViewModel(AuthorViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override (Character model, CharacterNode node) GetTestCharacter()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AssertUpdatedAuthorViewModel(AuthorViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override CharacterViewModel GetTestCharacterViewModel()
         {
             throw new NotImplementedException();
         }
@@ -30,62 +55,7 @@ namespace Bas.RedYarn.WebApp.Tests
             throw new NotImplementedException();
         }
 
-        protected override void AssertDiagram(Diagram model)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void AssertDiagramViewModel(DiagramViewModel viewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void AssertStoryline(Storyline model, StorylineNode node)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void AssertStorylineViewModel(StorylineViewModel viewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void AssertUpdatedAuthorViewModel(AuthorViewModel viewModel)
-        {
-            throw new NotImplementedException();
-        }
-
         protected override void AssertUpdatedCharacterViewModel(CharacterViewModel viewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void AssertUpdatedDiagramViewModel(DiagramViewModel viewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void AssertUpdatedStorylineViewModel(StorylineViewModel viewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override Author GetTestAuthor()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override AuthorViewModel GetTestAuthorViewModel()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override (Character model, CharacterNode node) GetTestCharacter()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override CharacterViewModel GetTestCharacterViewModel()
         {
             throw new NotImplementedException();
         }
@@ -100,12 +70,42 @@ namespace Bas.RedYarn.WebApp.Tests
             throw new NotImplementedException();
         }
 
+        protected override void AssertDiagram(Diagram model)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AssertDiagramViewModel(DiagramViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AssertUpdatedDiagramViewModel(DiagramViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
         protected override (Storyline model, StorylineNode node) GetTestStoryline()
         {
             throw new NotImplementedException();
         }
 
         protected override StorylineViewModel GetTestStorylineViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AssertStoryline(Storyline model, StorylineNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AssertStorylineViewModel(StorylineViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AssertUpdatedStorylineViewModel(StorylineViewModel viewModel)
         {
             throw new NotImplementedException();
         }
