ce3527da20c2247f37cf412baccf849dd01751c8
Bas Paap <baspaap@live.nl>
2018-10-30 16:57:23 +0100

    StoryDiagram now always reloads.

--- Bas.RedYarn.WebApp/ClientApp/src/app/story-diagram/story-diagram.component.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/story-diagram/story-diagram.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { DataSet } from 'vis';
 import { DiagramService } from '../diagram.service';
 import { VisNetworkGeneratorService } from '../vis-network-generator.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Diagram } from '../diagram-types';
 
 @Component({
@@ -14,15 +14,17 @@ export class StoryDiagramComponent implements OnInit {
 
   diagram: Diagram;
   graphData = {};
-    
+
   constructor(private route: ActivatedRoute,
-              private diagramService: DiagramService,
-              private visNetworkGeneratorService: VisNetworkGeneratorService) {    
+    private router: Router,
+    private diagramService: DiagramService,
+    private visNetworkGeneratorService: VisNetworkGeneratorService) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false; // Force Angular to reload even if only the parameters change.
   }
 
   ngOnInit() {
   }
-    
+
   ngAfterContentInit() {
     this.graphData["nodes"] = new DataSet();
     this.graphData["edges"] = new DataSet();
@@ -30,7 +32,7 @@ export class StoryDiagramComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.diagramService.getDiagram(id).subscribe((diagram: Diagram) => {
       this.diagram = diagram;
-      this.visNetworkGeneratorService.generate(diagram, this.graphData["nodes"], this.graphData["edges"]);      
+      this.visNetworkGeneratorService.generate(diagram, this.graphData["nodes"], this.graphData["edges"]);
     }, error => console.error(error));
   }
 }
