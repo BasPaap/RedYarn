85e46f8f1eca2b59572909a21d0c2bbea177a00d
Bas Paap <baspaap@live.nl>
2018-11-01 12:30:46 +0100

    Let NewCharacterDialog extend Dialog

--- Bas.RedYarn.WebApp/ClientApp/src/app/new-character-dialog/new-character-dialog.component.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/new-character-dialog/new-character-dialog.component.ts
@@ -2,52 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { DiagramService } from '../diagram.service';
+import { DialogComponent } from '../dialog.component';
 
 @Component({
   selector: 'app-new-character-dialog',
   templateUrl: './new-character-dialog.component.html',
   styleUrls: ['./new-character-dialog.component.scss']
 })
-export class NewCharacterDialogComponent implements OnInit {
-
-  newCharacterForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    description: new FormControl('')
-  });
-
+export class NewCharacterDialogComponent extends DialogComponent implements OnInit {
+  
   aliases: string[] = [];
   authors: string[] = [];
-  isSubmitting: boolean = false;
 
-  disableFormControls() {
-    for (let key in this.newCharacterForm.controls) {
-      if (this.newCharacterForm.controls[key].disabled) {
-        this.newCharacterForm.controls[key].enable();
-      }
-      else {
-        this.newCharacterForm.controls[key].disable();
-      }
+  createCharacter(): void {
+    if (this.formGroup.valid) {
+      this.toggleSubmitting();
+
+      let characterViewModel = {
+        id: "00000000-0000-0000-0000-000000000000",
+        name: this.formGroup.controls['name'].value,
+        description: this.formGroup.controls['description'].value,
+        aliases: this.aliases,
+        xPosition: 0,
+        yPosition: 0
+      };
+
+      this.diagramService.createCharacter(characterViewModel)
+        .subscribe(() => this.dialogRef.close());
     }
   }
 
-  createCharacter(): void {
-    this.isSubmitting = true;
-    this.disableFormControls();
-
-    let characterViewModel = {
-      id: "00000000-0000-0000-0000-000000000000",
-      name: this.newCharacterForm.controls['name'].value,
-      description: this.newCharacterForm.controls['description'].value,
-      aliases: this.aliases,
-      xPosition: 0,
-      yPosition: 0
-    };
-
-    this.diagramService.createCharacter(characterViewModel)
-      .subscribe(() => this.dialogRef.close());
-  }
-
   constructor(public dialogRef: MatDialogRef<NewCharacterDialogComponent>, private diagramService: DiagramService) {
+    super();
+    this.formGroup.addControl('name', new FormControl('', [Validators.required]));
+    this.formGroup.addControl('description', new FormControl(''));
   }
 
   ngOnInit() {
