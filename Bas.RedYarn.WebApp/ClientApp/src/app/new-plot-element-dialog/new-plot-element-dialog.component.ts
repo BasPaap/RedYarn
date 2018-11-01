31d3756aef8beeb096f719db2d258659e35d0245
Bas Paap <baspaap@live.nl>
2018-11-01 13:35:46 +0100

    NewPlotElementDialog now extends Dialog.

--- Bas.RedYarn.WebApp/ClientApp/src/app/new-plot-element-dialog/new-plot-element-dialog.component.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/new-plot-element-dialog/new-plot-element-dialog.component.ts
@@ -2,40 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 import { DiagramService } from '../diagram.service';
+import { DialogComponent } from '../dialog.component';
 
 @Component({
   selector: 'app-new-plot-element-dialog',
   templateUrl: './new-plot-element-dialog.component.html',
   styleUrls: ['./new-plot-element-dialog.component.scss']
 })
-export class NewPlotElementDialogComponent implements OnInit {
-
-  newPlotElementForm = new FormGroup({
-    name: new FormControl('',  [Validators.required]),
-    description: new FormControl('')
-  });
-
-  isSubmitting: boolean = false;
-
-  disableFormControls() {
-    for (let key in this.newPlotElementForm.controls) {
-      if (this.newPlotElementForm.controls[key].disabled) {
-        this.newPlotElementForm.controls[key].enable();
-      }
-      else {
-        this.newPlotElementForm.controls[key].disable();
-      }
-    }
-  }
+export class NewPlotElementDialogComponent extends DialogComponent implements OnInit {
 
   createPlotElement(): void {
-    this.isSubmitting = true;
-    this.disableFormControls();
-
+    
     let plotElementViewModel = {
       id: "00000000-0000-0000-0000-000000000000",
-      name: this.newPlotElementForm.controls['name'].value,
-      description: this.newPlotElementForm.controls['description'].value,
+      name: this.formGroup.controls['name'].value,
+      description: this.formGroup.controls['description'].value,
       xPosition: 0,
       yPosition: 0
     };
@@ -44,7 +25,12 @@ export class NewPlotElementDialogComponent implements OnInit {
       .subscribe(() => this.dialogRef.close());
   }
 
-  constructor(public dialogRef: MatDialogRef<NewPlotElementDialogComponent>, private diagramService: DiagramService) { }
+  constructor(public dialogRef: MatDialogRef<NewPlotElementDialogComponent>, private diagramService: DiagramService) {
+    super();
+
+    this.formGroup.addControl('name', new FormControl('', [Validators.required]));
+    this.formGroup.addControl('description');
+  }
 
   ngOnInit() {
   }
