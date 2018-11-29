f92c8c8e771f943345d522fd99d27df6909d4a51
Bas Paap <baspaap@live.nl>
2018-11-29 15:28:59 +0100

    Fix typo

--- Bas.RedYarn.WebApp/ClientApp/src/app/new-relationship-ui.service.spec.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/new-relationship-ui.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { NewRelationshipUiService } from './new-relationship-ui.service';
+import { NewRelationshipUIService } from './new-relationship-ui.service';
 
 describe('NewRelationshipUiService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
