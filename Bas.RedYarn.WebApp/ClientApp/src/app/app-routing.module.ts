40eba46962d2d238f46979113c3d9d147d706a6f
Bas Paap <baspaap@live.nl>
2018-12-20 13:17:11 +0100

    Implement opening and closing of the sidenav when navigating to/from a diagram.

--- Bas.RedYarn.WebApp/ClientApp/src/app/app-routing.module.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoryDiagramComponent } from './components/story-diagram/story-diagram.component';
+import { MyDiagramsComponent } from './components/my-diagrams/my-diagrams.component';
 
 const routes: Routes = [
-  { path: 'diagram', component: StoryDiagramComponent },
+  { path: 'diagram', component: MyDiagramsComponent },
   { path: 'diagram/:id', component: StoryDiagramComponent }
 ];
 
--- Bas.RedYarn.WebApp/ClientApp/src/app/app.component.ts
+++ Bas.RedYarn.WebApp/ClientApp/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { UserInteractionService } from './services/user-interaction.service';
 import { SettingsService } from './services/settings.service';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,24 @@ import { SettingsService } from './services/settings.service';
 export class AppComponent {
   title = 'RedYarn';
 
-  constructor(private userInteractionService: UserInteractionService, private settingsService: SettingsService) {
+  @ViewChild(MatSidenav) sideNav: MatSidenav;
+
+  constructor(private userInteractionService: UserInteractionService, private settingsService: SettingsService, private router: Router) {
     this.settingsService.load();
+
+    router.events.subscribe(event => {
+      // If we are navigating to a diagram, open the sidenav.
+      if (event instanceof NavigationEnd && event.urlAfterRedirects.startsWith("/diagram/")) {
+        this.sideNav.open();
+      }
+
+      // If we are looking at a diagram, but are navigating away, close the sidenav.
+      if (event instanceof NavigationStart && this.router.url.startsWith("/diagram/")) {
+        this.sideNav.close();
+      }
+    });
   }
-  
+
   @HostListener('mousedown') onMouseDown() {
     this.userInteractionService.onMouseDown();
   }
@@ -21,7 +37,7 @@ export class AppComponent {
   @HostListener('mouseup') onMouseUp() {
     this.userInteractionService.onMouseUp;
   }
-  
+
   @HostListener('mousemove', ['$event.clientX', '$event.clientY', '$event.buttons'])
   onMouseMove(x: number, y: number, buttons: number) {
     this.userInteractionService.onMouseMove(x, y, buttons);
